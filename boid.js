d939a77807513867f1c7cdcb7b426b0861855484
sbuggay <sbuggay@gmail.com>
2020-02-23 16:53:17 -0800

    Fix perception visualization

--- boid.js
+++ boid.js
@@ -3,10 +3,11 @@ class Boid {
         this.position = createVector(random(width), random(height));
         this.velocity = p5.Vector.random2D();
         this.acceleration = createVector();
-        this.maxSpeed = 5;
-        this.maxForce = 0.1;
+        this.maxSpeed = 3;
+        this.maxForce = 0.01;
         this.size = 15;
-        this.perception = 50;
+        this.perception = 30;
+        this.affected = false;
     }
 
     align(boids) {
@@ -28,7 +29,7 @@ class Boid {
             wish.setMag(this.maxSpeed);
             wish.sub(this.velocity);
             wish.limit(this.maxForce);
-
+            this.affected = true;
         }
         return wish;
     }
@@ -52,17 +53,17 @@ class Boid {
             wish.setMag(this.maxSpeed);
             wish.sub(this.velocity);
             wish.limit(this.maxForce);
+            this.affected = true;
         }
         return wish;
     }
 
     update(boids) {
 
+        this.affected = false;
         this.acceleration.add(this.align(boids));
         this.acceleration.add(this.cohesion(boids));
         
-        
-
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
         this.velocity.limit(this.maxSpeed);
@@ -80,12 +81,14 @@ class Boid {
         push();
         translate(this.position.x, this.position.y)
         rotate(this.velocity.heading());
-        fill(51);
+        fill(0, 0, 0, 0);
         strokeWeight(1);
         stroke(255);
         
         const h = Math.sqrt(this.size ** 2 - (this.size / 2) ** 2);
         triangle(h / 2, 0, h / -2, this.size / 3, h / -2, this.size / -3);
+        stroke(this.affected ? color(0, 100, 0) : 100);
+        circle(0, 0, this.perception * 2);
         pop();
     }
 }
