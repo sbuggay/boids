a9126610f6f44517365874657c610529b36dcddc
sbuggay <sbuggay@gmail.com>
2020-02-24 13:24:43 -0800

    Optimizations

--- boid.js
+++ boid.js
@@ -8,97 +8,91 @@ class Boid {
         this.position = createVector(random(width), random(height));
         this.velocity = p5.Vector.random2D();
         this.acceleration = createVector();
-        this.perception = 50;
+        this.perception = 30;
         this.affected = false;
     }
 
     align(boids) {
+        if (boids.length === 0) return;
+
         let wish = createVector();
 
-        let count = 0;
-
         for (let boid of boids) {
-            if (boid === this) continue;
-
-            if (dist(this.position.x, this.position.y, boid.position.x, boid.position.y) < this.perception) {
-                wish.add(boid.velocity);
-                count++;
-            }
-
-            if (count >= maxPercieved) break;
+            wish.add(boid.velocity);
         }
 
-        if (count > 0) {
-            wish.div(count);
-            wish.setMag(maxSpeed);
-            wish.sub(this.velocity);
-            wish.limit(maxForce);
-            this.affected = true;
-        }
+        wish.div(boids.length);
+        wish.setMag(maxSpeed);
+        wish.sub(this.velocity);
+        wish.limit(maxForce);
         return wish;
     }
 
     cohesion(boids) {
+        if (boids.length === 0) return;
+
         let wish = createVector();
 
-        let count = 0;
-
         for (let boid of boids) {
-            if (boid === this) continue;
-
-            if (dist(this.position.x, this.position.y, boid.position.x, boid.position.y) < this.perception) {
-                wish.add(boid.position);
-                count++;
-            }
-
-            if (count >= maxPercieved) break;
+            wish.add(boid.position);
         }
 
-        if (count > 0) {
-            wish.div(count);
-            wish.sub(this.position);
-            wish.setMag(maxSpeed);
-            wish.sub(this.velocity);
-            wish.limit(maxForce);
-            this.affected = true;
-        }
+        wish.div(boids.length);
+        wish.sub(this.position);
+        wish.setMag(maxSpeed);
+        wish.sub(this.velocity);
+        wish.limit(maxForce);
+
         return wish;
     }
 
-    separation(boids) {
+    separation(boids, d) {
+        if (boids.length === 0) return;
+
         let wish = createVector();
-        let count = 0;
+
         for (let boid of boids) {
-            if (boid === this) continue;
             let d = dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (d < (this.perception / 2)) {
+            // if (d < (this.perception / 2)) {
                 let diff = p5.Vector.sub(this.position, boid.position);
                 diff.div(d * d);
                 wish.add(diff);
-                count++;
-            }
+            // }
+        }
 
-            if (count >= maxPercieved) break;
-        }
-        if (count > 0) {
-            wish.div(count);
-            wish.setMag(maxSpeed * 2);
-            wish.sub(this.velocity);
-            wish.limit(maxForce);
-        }
+        wish.div(boids.length);
+        wish.setMag(maxSpeed * 1.1);
+        wish.sub(this.velocity);
+        wish.limit(maxForce);
+
         return wish;
     }
 
     update(boids) {
 
-        this.affected = false;
-        this.acceleration.add(this.align(boids));
-        this.acceleration.add(this.cohesion(boids));
-        this.acceleration.add(this.separation(boids));
+        let count = 0;
+
+        let localBoids = [];
+
+        for (let boid of boids) {
+            if (boid === this) continue;
+            let d = (this.position.x - boid.position.x) ** 2 + (this.position.y - boid.position.y) ** 2;
+            if (d < (this.perception) ** 2) {
+                localBoids.push(boid);
+
+            }
+            count++;
+            if (count >= maxPercieved) break;
+        }
+
+        this.acceleration.add(this.align(localBoids));
+        this.acceleration.add(this.cohesion(localBoids));
+        this.acceleration.add(this.separation(localBoids));
+
+        this.affected = localBoids.length > 0;
 
         if (window.mouse) {
             if (window.mouse.x <= width && window.mouse.x >= 0 && window.mouse.y <= height && window.mouse.y >= 0) {
-
                 const wish = p5.Vector.sub(createVector(window.mouse.x, window.mouse.y), this.position);
                 this.acceleration.add(wish);
             }
@@ -125,12 +119,13 @@ class Boid {
         strokeWeight(1);
         stroke(255);
 
-        const h = Math.sqrt(size ** 2 - (size / 2) ** 2);
-        triangle(h / 2, 0, h / -2, size / 3, h / -2, size / -3);
-
         if (window.debug === 2) {
             stroke(this.affected ? color(0, 100, 0) : 100);
             circle(0, 0, this.perception * 2);
+        }
+        else {
+            const h = Math.sqrt(size ** 2 - (size / 2) ** 2);
+            triangle(h / 2, 0, h / -2, size / 3, h / -2, size / -3);
         }
 
         pop();
--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 let flock = [];
 let quadtree;
-window.debug = 0; // 0, 1, 2, 3
+let fps = 0;
+
 
 function setup() {
-    
     const width = Math.min(800, windowWidth);
-
     const canvas = createCanvas(width, width);
     canvas.parent("sketch");
-
     reset();
-
 }
 
 function reset() {
+    window.debug = 0; // 0, 1, 2, 3
+    window.pause = false;
     flock = [];
-    for (let i = 0; i < 250; i++) {
+    for (let i = 0; i < 300; i++) {
         const boid = new Boid()
         flock.push(boid);
     }
@@ -33,13 +32,24 @@ function draw() {
     for (let boid of flock) {
         let range = new Rect(boid.position.x, boid.position.y, boid.perception, boid.perception);
         let localFlock = quadtree.query(range);
-        boid.update(localFlock);
+
+        if (!window.pause)
+            boid.update(localFlock);
+
         boid.render();
     }
 
     if (window.debug === 1) {
         quadtree.render();
     }
+
+    setInterval(() => {
+        fps = frameRate();
+    }, 100);
+
+    fill(fps >= 60 ? "green" : "red");
+    stroke(0);
+    text(fps.toFixed(2), 10, 20);
 }
 
 debugButton.onclick = () => {
@@ -48,10 +58,13 @@ debugButton.onclick = () => {
 
 function keyReleased() {
     if (key === "d") {
-        window.debug = (window.debug + 1) % 2;
+        window.debug = (window.debug + 1) % 3;
     }
     else if (key === "r") {
         reset();
+    }
+    else if (key === "p") {
+        window.pause = !window.pause;
     }
     return false; // prevent any default behavior
 }
