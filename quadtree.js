31ac80a41be5cb4bd341e0c6f1b6b68b3b13e72f
sbuggay <sbuggay@gmail.com>
2020-02-23 17:51:05 -0800

    Add basic quadtree

--- quadtree.js
+++ quadtree.js
@@ -12,9 +12,76 @@ class Rect {
         this.w = w;
         this.h = h;
     }
+
+    contains(point) {
+        return (point.x >= this.x - this.w &&
+            point.x < this.x + this.w &&
+            point.y >= this.y - this.h &&
+            point.y < this.y + this.h);
+    }
 }
 
 class QuadTree {
-    
+    constructor(boundary, capacity) {
+        this.boundary = boundary;
+        this.capacity = capacity;
+        this.list = [];
+        this.divided = false;
+    }
+
+    insert(point) {
+
+        if (!this.boundary.contains(point)) return;
+
+        if (this.list.length < this.capacity) {
+            this.list.push(point);
+        }
+        else {
+            if (!this.divided) {
+                this.subdivide();
+            }
+
+            this.ne.insert(point);
+            this.nw.insert(point);
+            this.sw.insert(point);
+            this.se.insert(point);
+        }
+    }
+
+    subdivide() {
+        let x = this.boundary.x;
+        let y = this.boundary.y;
+        let w = this.boundary.w;
+        let h = this.boundary.h;
+
+        let neRect = new Rect(x + w / 2, y + h / 2, w / 2, h / 2);
+        let nwRect = new Rect(x - w / 2, y + h / 2, w / 2, h / 2);
+        let swRect = new Rect(x - w / 2, y - h / 2, w / 2, h / 2);
+        let seRect = new Rect(x + w / 2, y - h / 2, w / 2, h / 2);
+        this.ne = new QuadTree(neRect, this.capacity);
+        this.nw = new QuadTree(nwRect, this.capacity);
+        this.sw = new QuadTree(swRect, this.capacity);
+        this.se = new QuadTree(seRect, this.capacity);
+        this.divided = true;
+
+    }
+
+    render() {
+
+        if (this.ne) this.ne.render();
+        if (this.nw) this.nw.render();
+        if (this.se) this.se.render();
+        if (this.sw) this.sw.render();
+
+        push();
+        stroke(200);
+        fill(0, 0, 0, 0);
+        rectMode(CENTER);
+        translate(this.boundary.x, this.boundary.y);
+        rect(0, 0, this.boundary.w * 2, this.boundary.h * 2)
+        pop();
+
+
+    }
 }
 
--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const flock = [];
+let qt;
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
+function setup() {  
+    createCanvas(800, 800);
 
-    for (let i = 0; i < 100; i++) {
-        flock.push(new Boid());
+
+    for (let i = 0; i < 200; i++) {
+        const boid = new Boid()
+        flock.push(boid);
+
     }
+
+    
 }
 
 function draw() {
     background(51);
 
+    qt = new QuadTree(new Rect(width / 2, height / 2, width / 2, height / 2), 2);
+
+
     for (let boid of flock) {
         boid.update(flock);
+        qt.insert(boid.position);
         boid.render();
     }
+
+    qt.render();
 }
